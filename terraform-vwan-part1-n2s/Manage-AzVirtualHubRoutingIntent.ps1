66de3ae02d9834517fff91d875a12e400a5f1bd4
John McDonough <movinalot@gmail.com>
2024-09-25 16:34:59 -0400

    manage vhub components

---
+++ terraform-vwan-part1-n2s/Manage-AzVirtualHubRoutingIntent.ps1
@@ -0,0 +1,42 @@
+<#
+    .DESCRIPTION
+        Remove Azure Virtual Hub Routing Intent
+
+    .NOTES
+        AUTHOR: jmcdonough@fortinet.com
+        LASTEDIT: September 25, 2024
+
+	.Examples
+		List
+		./Manage-AzVirtualHubRoutingIntent.ps1 -ResourceGroups @($(Get-AzResourceGroup -Name lab-chile-?-training))
+
+		List and Delete
+		./Manage-AzVirtualHubRoutingIntent.ps1 -ResourceGroups @($(Get-AzResourceGroup -Name lab-chile-?-training)) -Delete
+
+#>
+
+param(
+	[CmdletBinding()]
+
+	[Parameter(Mandatory = $true)]
+	[Array] $ResourceGroups,
+
+	[Parameter(Mandatory = $false)]
+	[switch] $Delete
+)
+
+foreach ($resourceGroup in $ResourceGroups) {
+	$vHubs = Get-AzVirtualHub -ResourceGroupName $resourceGroup.ResourceGroupName
+
+	foreach ($vhub in $vhubs) {
+		$routingIntent = Get-AzRoutingIntent -ResourceGroupName $resourceGroup.ResourceGroupName -HubName $vHub.Name -ErrorAction SilentlyContinue
+		if ($routingIntent) {
+			if ($Delete) {
+                Write-Host "Deleting vHub Routing Intent - Resource Group: $($resourceGroup.ResourceGroupName) vHub: $($vHub.Name) - $($routingIntent.Id)"
+				Remove-azroutingIntent -ResourceId $routingIntent.Id -AsJob -Force
+			} else {
+                Write-Host "vHub Routing Intent - Resource Group: $($resourceGroup.ResourceGroupName) vHub: $($vHub.Name) - $($routingIntent.Id)"
+            }
+		}
+	}
+}
